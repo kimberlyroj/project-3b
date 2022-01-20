3b11aa768c6220d055c47fa9b675515b79b6b07b
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-19 23:06:41 -0800

    update

--- factors.py
+++ factors.py
@@ -1,3 +1,7 @@
+# Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 1/18/2021
+# Description: Write a program that asks the user to enter a positive integer, then prints a list of all positive integers that divide that number evenly, including itself and 1, in ascending order
 num = int(input("Please enter a positive integer:"))
 print("the factors of", num, "are")
 for i in range(1,num+1,1):
